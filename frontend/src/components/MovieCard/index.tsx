da675a7b540c3deaed02e5b8c33c7e5a06d90c8a
Marcela Pereira <marcelaompereira@gmail.com>
2022-01-16 23:01:10 -0300

    Props

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,18 +1,15 @@
 import MovieScore from "components/MovieScore";
 import { Link } from "react-router-dom";
+import { Movie } from "types/movie";
 // usando mesmo css do Form
 // css é global, uma vez que importou em algum componente ele vai estar disponível,
 // pegou css do Form
 
-function MovieCard() {
+type Props = {
+    movie: Movie;
+}
 
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+function MovieCard({ movie } : Props) {
 
     return (
         <div>
